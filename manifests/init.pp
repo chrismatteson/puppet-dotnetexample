7adbc52606abe4f5c0f1a86150d328197395e3d9
root <root@master.inf.puppetlabs.demo>
2016-03-09 05:51:19 +0000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,60 @@
+class dotnetexample (
+  $websitename        = 'DotNetExample',
+  $zipname            = '16781.zip',
+  $sourcerepo         = 'http://buildtool.mcafee.com/PackageDownload/QA/',
+  $destinationpath    = 'D:\inetpub\platformutil.mcafee.com-distribution\Utilities',
+  $defaultwebsitepath = 'C:\inetpub\wwwroot',
+  $zippath            = 'C:\tmp'
+){
+
+  $zipuri  = "${sourcerepo}/${zipname}"
+  $zipfile = "${zippath}\\${zipname}"
+
+  # Install the IIS role
+  dsc_windowsfeature {'IIS':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Server',
+  } ->
+
+  # Install the ASP .NET 4.5 role
+  dsc_windowsfeature {'AspNet45':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Asp-Net45',
+  } ->
+
+  # Stop an existing website (set up in Sample_xWebsite_Default)
+  dsc_xwebsite {'Stop DefaultSite':
+    dsc_ensure       => 'present',
+    dsc_name         => 'Default Web Site',
+    dsc_state        => 'Stopped',
+    dsc_physicalpath => $defaultwebsitepath,
+  } ->
+
+  # Create tmp folder
+  dsc_file {'tmp folder':
+    dsc_ensure          => 'present',
+    dsc_type            => 'Directory',
+    dsc_destinationpath => $zippath,
+  } ->
+
+  # Download the site content
+  dsc_xremotefile {'Download WebContent Zip':
+    dsc_destinationpath => $zipfile,
+    dsc_uri             => $zipuri,
+  } ->
+
+  # Extract the website content 
+  dsc_archive {'Unzip and Copy the WebContent':
+    dsc_ensure      => 'present',
+    dsc_path        => $zipfile,
+    dsc_destination => $destinationpath,
+  } ->
+
+  # Create a new App Pool
+  dsc_xwebapppool {'DotNetWebSite':
+    dsc_ensure       => 'present',
+    dsc_name         => $websitename,
+    dsc_state        => 'Started',
+    dsc_physicalpath => $destinationpath,
+  }
+}
